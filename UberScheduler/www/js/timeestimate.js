9d1fc8cf8a6c046e19187ce8573e6e96ec4e469b
Adam Barr-Neuwirth <adambarrneuwirth@gmail.com>
2016-01-12 11:18:01 -0800

    use our server

--- UberScheduler/www/js/timeestimate.js
+++ UberScheduler/www/js/timeestimate.js
@@ -5,19 +5,22 @@ function testimate() {
 
 var slat = alat;
 var slong = along;
-var url = "https://sandbox-api.uber.com/v1/estimates/time?server_token=ikGvlAJSejPSY6bUp7APhxkwyu5ermguZnreUaCd&start_latitude=" + slat + "&start_longitude=" + slong;
+var url = "http://uberscheduler-1184.appspot.com/main.py";
     
-xhr = new XMLHttpRequest();
-xhr.open('GET', url);
-xhr.setRequestHeader("Authorization", "Token ikGvlAJSejPSY6bUp7APhxkwyu5ermguZnreUaCd");
-xhr.send();
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-      var json = JSON.stringify(eval("(" + xhr.responseText + ")"));
-      json=JSON.parse(json);
-      console.log(json);   
-    document.getElementById("estimate").innerHTML = "Next UberX in seconds: " + json['times'][0]['estimate'];
-  }
-
-}
+//xhr.open('GET', url);
+//xhr.setRequestHeader("Authorization", "Token ikGvlAJSejPSY6bUp7APhxkwyu5ermguZnreUaCd");
+//xhr.send();
+//xhr.onreadystatechange = function() {
+//  if (xhr.readyState == 4 && xhr.status == 200) {
+//      var json = JSON.stringify(eval("(" + xhr.responseText + ")"));
+//      json=JSON.parse(json);
+//      console.log(json);   
+//    document.getElementById("estimate").innerHTML = "Next UberX in seconds: " + json['times'][0]['estimate'];
+//  }
+//
+//}
+xhttp = new XMLHttpRequest();
+xhttp.open("GET", url, true);
+xhttp.send();
+console.log(xhttp.responseText);
 }

ff819b9400f4e6f84e9872197df2d52a1af4026c
Adam Barr-Neuwirth <adambarrneuwirth@gmail.com>
2016-01-28 19:41:30 -0800

    query attempts

--- Python Server/Uber/datastore.py
+++ Python Server/Uber/datastore.py
@@ -16,24 +16,33 @@ class UserID(ndb.Model):
 pickupLocation = [34.07636433,-118.4290661]
 dropLocation = [34.07636433,-118.4290661]
 
-slat = pickupLocation[0]
-slong = pickupLocation[1]
-elat = dropLocation[0]
-elong = dropLocation[1]
+sslat = pickupLocation[0]
+sslong = pickupLocation[1]
+eelat = dropLocation[0]
+eelong = dropLocation[1]
 
 class Ride(ndb.Model):
     """A main model for representing an individual Guestbook entry."""
     uid = ndb.StructuredProperty(UserID)
-    slong = ndb.StringProperty(indexed=False)
-    slat = ndb.StringProperty(indexed=False)
-    elong = ndb.StringProperty(indexed=False)
-    elat = ndb.StringProperty(indexed=False)
+    slong = ndb.FloatProperty(indexed=False)
+    slat = ndb.FloatProperty(indexed=False)
+    elong = ndb.FloatProperty(indexed=False)
+    elat = ndb.FloatProperty(indexed=False)
     time = ndb.StringProperty()
+    date = ndb.StringProperty()
 
-    date = ndb.DateTimeProperty(auto_now_add=True)
 
-
+#INSERT INTO DATASTORE
 user = UserID(id=1, email="swag@bomb.com", passwd="mcswag1n")
-user_key = user.put();
+ride = Ride(uid=user, slong=sslong, slat=sslat, elong=eelong, elat=eelat, time="12:00", date="012816")
+ride_key = ride.put()
+print ride_key
+
+
+
+#RETRIEVE FROM DATASTORE
+qry = Ride.query(Ride.uid.id < 5)
+print qry.key
+
 
 #YEAH THATS FUCKING RIGHT THIS WORKS

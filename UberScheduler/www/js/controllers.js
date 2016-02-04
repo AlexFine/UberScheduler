def01d494de4138fd0c011ed60002faa19f48a5b
Kevin Hou <kevin.ch.hou@gmail.com>
2016-02-03 21:29:22 -0800

    Fixed bug that would happen when no days of the week were selected

--- UberScheduler/www/js/controllers.js
+++ UberScheduler/www/js/controllers.js
@@ -104,6 +104,7 @@ angular.module('starter.controllers', ['ui.bootstrap'])
 
     $scope.findNextRide = function(rideTime, daysOfWeek) { //Now using node package: 'later'
       //Doesn't incorporate start/end date of schedule
+
       var arrayOfDays = [];
       for (var i = 0; i < daysOfWeek.length; i++) {
         if (daysOfWeek[i]) { //if that day is true
@@ -112,15 +113,22 @@ angular.module('starter.controllers', ['ui.bootstrap'])
       }
       // console.log("Days of week:", arrayOfDays)
 
+      /*  Check if there is indeed a recurring schedule  */
+      if (arrayOfDays.length == 0) { //If there is no recurring part of the schedule
+        return undefined; //Return nothing
+      }
+
       var hour = new Date(rideTime).getHours();
-      console.log("Hour:", hour);
+      // console.log("Hour:", hour);
+
+      //It always sets the hours 8 hours too few after compiling the schedule
       if (hour <= 16) {
         hour += 8; //Add 8 to compensate
       } else {
         var num = hour + 8;
         hour = num % 24;
       }
-      console.log("Updated hour:", hour);
+      // console.log("Updated hour:", hour);
 
       var minutes = new Date(rideTime).getMinutes();
 
@@ -140,16 +148,15 @@ angular.module('starter.controllers', ['ui.bootstrap'])
       // console.log(compiledSchedule);
       var nextRide = compiledSchedule.next(1); //Next instance of schedule
 
-      //It always sets the hours 8 hours too few after compiling the schedule
       if (nextRide.getHours() >= 16) { //Compensating because it's so jank
         nextRide.setDate(nextRide.getDate() + 1); //Add a day
         var previousRide = compiledSchedule.prev(1);
         previousRide.setDate(previousRide.getDate() + 1);
         var currentDate = new Date();
-        console.log(previousRide.getTime())
-        console.log(currentDate.getTime())
+        // console.log(previousRide.getTime())
+        // console.log(currentDate.getTime())
         if (previousRide.getTime() > currentDate.getTime()) { //If the previous ride should be the current ride
-          console.log(previousRide)
+          // console.log(previousRide)
           nextRide = previousRide;
         }
       }

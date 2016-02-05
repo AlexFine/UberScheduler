8db40b67d2836270e2b3668b70430adf94a3b812
Kevin Hou <kevin.ch.hou@gmail.com>
2016-02-05 13:19:43 -0800

    Added function to prep data before it is sent to the server

--- UberScheduler/www/js/controllers.js
+++ UberScheduler/www/js/controllers.js
@@ -248,38 +248,64 @@ angular.module('starter.controllers', ['ui.bootstrap'])
           showOptions: true //Show options when adding new schedule
         }
       )
-    }
+    };
+
+    $scope.pushRidesToServer = function() {
+
+      /*********      Conversions     *********/
+      //Convert the nextRide variable into a number readable by the server
+      //Convert the time variable into an integer
+      var data = $scope.scheduledRides; //The data variable will be sent
+      for (var i = 0; i < data.length; i++) {
+        //time conversion
+        var time = data[i].time;
+        var ray = [time.getHours(), time.getMinutes()];
+        data[i].time = ray; //Assign to object
+
+        //nextRide conversion
+        var nextRide = data[i].nextRide;
+        var num = Date.parse(nextRide);
+        data[i].nextRide = num; //Assign to object
+      };
+      /*********      Conversion Complete     *********/
+      console.log("Sending data:") //Feedback
+      console.log(data)
+
+      /*********      Pushing to Server     *********/
+    };
 
     $scope.UberTypes = ["UberX", "UberBlack", "UberBlack", "ACCESS"];
     $scope.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     $scope.scheduledRides = [
         {
-            time: new Date(2016, 0, 1, 2, 3, 4, 567),
-            pickupLocation: [34.07636433, -118.4290661],
-            pickupName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"], //Will declare this variable when the location is selected on the map
-            dropLocation: [34.07636433, -118.4290661],
-            dropName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"],
-            repeatedDays: [false, true, false, true, false, true, false],
-            startDate: new Date(2016, 01, 01), //Months indexed from 0
-            endDate: new Date(2016, 2, 01),
-            nextRide: new Date($scope.findNextRide(new Date(2016, 0, 1, 2, 3, 4, 567), [false, true, false, true, false, true, false])), //Calculated on spot because these are tests
-            product: 1,
-            showOptions: true
-          },
-          {
-            time: new Date(2016, 0, 1, 18, 32, 5, 567),
-            pickupLocation: [34.07636433, -118.4290661],
-            pickupName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"],
-            dropLocation: [34.07636433, -118.4290661],
-            dropName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"],
-            repeatedDays: [false, true, false, true, false, true, false],
-            startDate: new Date(2016, 01, 01),
-            endDate: new Date(2016, 4, 01),
-            nextRide: new Date($scope.findNextRide(new Date(2016, 0, 1, 18, 32, 5, 567), [false, true, false, true, false, true, false])),
-            product: 2,
-            showOptions: false
-          }
-        ];
+          time: new Date(2016, 0, 1, 2, 3, 4, 567),
+          pickupLocation: [34.07636433, -118.4290661],
+          pickupName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"], //Will declare this variable when the location is selected on the map
+          dropLocation: [34.07636433, -118.4290661],
+          dropName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"],
+          repeatedDays: [false, true, false, true, false, true, false],
+          startDate: new Date(2016, 01, 01), //Months indexed from 0
+          endDate: new Date(2016, 2, 01),
+          nextRide: new Date($scope.findNextRide(new Date(2016, 0, 1, 2, 3, 4, 567), [false, true, false, true, false, true, false])), //Calculated on spot because these are tests
+          product: 1,
+          showOptions: true
+        },
+        {
+          time: new Date(2016, 0, 1, 18, 32, 5, 567),
+          pickupLocation: [34.07636433, -118.4290661],
+          pickupName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"],
+          dropLocation: [34.07636433, -118.4290661],
+          dropName: ["10236 Charing Cross Rd", "Los Angeles", "CA", "90024"],
+          repeatedDays: [false, true, false, true, false, true, false],
+          startDate: new Date(2016, 01, 01),
+          endDate: new Date(2016, 4, 01),
+          nextRide: new Date($scope.findNextRide(new Date(2016, 0, 1, 18, 32, 5, 567), [false, true, false, true, false, true, false])),
+          product: 2,
+          showOptions: false
+        }
+      ];
+
+      $scope.pushRidesToServer();
 
         //POPUP SEXY ACTION STUFF
         // Triggered on a button click, or some other target

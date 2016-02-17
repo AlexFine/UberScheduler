52300ccf0132329eb3124bf263c7642373d7f6df
AlexFine <alexkfine2@gmail.com>
2016-02-17 15:15:20 -0800

    Error need help working testing accordion

--- UberScheduler/www/js/controllers.js
+++ UberScheduler/www/js/controllers.js
@@ -497,7 +497,7 @@ angular.module('starter.controllers', ['ui.bootstrap'])
         });
 
         $scope.map = map;
-    }
+    
     google.maps.event.addDomListener(window, 'load', initialize());
 
     $scope.centerOnMe = function () {
@@ -523,8 +523,39 @@ angular.module('starter.controllers', ['ui.bootstrap'])
     $scope.clickTest = function () {
         alert('Will Launch Call Uber Window From here')
     };
+    
+    }
+    //ACCORDIAN CODE
+    function($scope) {
+  $scope.groups = [];
+  for (var i=0; i<10; i++) {
+    $scope.groups[i] = {
+      name: i,
+      items: []
+    };
+    for (var j=0; j<3; j++) {
+      $scope.groups[i].items.push(i + '-' + j);
+    }
+  }
+  
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+        //END OF ACORDIAN CODE 
 
-})
+}
+    })
 
 .controller('settingsCtrl', function ($scope, $stateParams) {
     $scope.editAddress = function () {

14b4ab2bd9520ca5e719f03a4c3e7fe60b28d1e6
Kevin Hou <kevin.ch.hou@gmail.com>
2016-02-24 15:46:27 -0800

    Made timeUntilDate a global function

--- UberScheduler/www/js/controllers.js
+++ UberScheduler/www/js/controllers.js
@@ -89,47 +89,7 @@ angular.module('starter.controllers', ['ui.bootstrap'])
         }
     };
 
-    $scope.timeUntilDate = function(a, returnType) {
-      var firstDate = new Date(); //Current time
-      var secondDate = new Date(a); //Input time
 
-      // http://stackoverflow.com/questions/8528382/javascript-show-milliseconds-as-dayshoursmins-without-seconds
-      var t = Math.abs(secondDate.getTime() - firstDate.getTime()); //Milliseconds between the time
-      var cd = 24 * 60 * 60 * 1000,
-      ch = 60 * 60 * 1000,
-      d = Math.floor(t / cd),
-      h = Math.floor( (t - d * cd) / ch),
-      m = Math.round( (t - d * cd - h * ch) / 60000),
-      pad = function(n){ return n < 10 ? '0' + n : n; };
-      if( m === 60 ){
-        h++;
-        m = 0;
-      }
-      if( h === 24 ){
-        d++;
-        h = 0;
-      }
-      // console.log([d, pad(h), pad(m)].join(':')); //Feedback
-      if (returnType == "string") {
-        var dayLabel = " days, ";
-        if (d == 1) { //If only one day
-          dayLabel = " day, ";
-        }
-        var hourLabel = " hours, ";
-        if (h == 1) { //If only one day
-          hourLabel = " hour, ";
-        }
-        var minuteLabel = " minutes";
-        if (m == 1) { //If only one day
-          minuteLabel = " minute";
-        }
-        return (
-          d +  dayLabel + h + hourLabel + m + minuteLabel
-        )
-      } else {
-        return [d, pad(h), pad(m)].join(':');
-      }
-    };
 
     $scope.reverseGeocode = function (lat, lng) {
         var geocoder = new google.maps.Geocoder;
@@ -455,8 +415,8 @@ angular.module('starter.controllers', ['ui.bootstrap'])
             alat = pos.coords.latitude;
             along = pos.coords.longitude;
             console.log(alat, along);
-            document.getElementById("lat").innerHTML = alat;
-            document.getElementById("long").innerHTML = along;
+            // document.getElementById("lat").innerHTML = alat;
+            // document.getElementById("long").innerHTML = along;
 
             // Set marker
             var marker = new google.maps.Marker({

a4020c9eea4e1361d74ba2bf6ede0c66ea5c8cb0
Adam Barr-Neuwirth <adambarrneuwirth@gmail.com>
2016-03-01 13:41:01 -0800

    auth2

--- Python Server/Uber/auth_2.py
+++ Python Server/Uber/auth_2.py
@@ -1,26 +1,26 @@
 import cgi
-import request
 import requests
-
+#from rauth import OAuth2Service
+from flask import request, url_f
 
 # Once your user has signed in using the previous step you should redirect
 # them here
+def go():
+    parameters = {
+        'redirect_uri': 'INSERT_ROUTE_TO_STEP_TWO',
+        'code': request.args.get('code'),
+        'grant_type': 'authorization_code',
+    }
 
-parameters = {
-    'redirect_uri': 'INSERT_ROUTE_TO_STEP_TWO',
-    'code': request.args.get('code'),
-    'grant_type': 'authorization_code',
-}
+    response = requests.post(
+        'https://login.uber.com/oauth/token',
+        auth=(
+            '_g8Fw1o5KsrVRNBNskv2cMEMhoE6RPbW',
+            'MOlViQFGozeOuCdADPhBBay7_Jz6Ac1YfqcZpTWT',
+        ),
+        data=parameters,
+    )
 
-response = requests.post(
-    'https://login.uber.com/oauth/token',
-    auth=(
-        '_g8Fw1o5KsrVRNBNskv2cMEMhoE6RPbW',
-        'MOlViQFGozeOuCdADPhBBay7_Jz6Ac1YfqcZpTWT',
-    ),
-    data=parameters,
-)
-
-# This access_token is what we'll use to make requests in the following
-# steps
-access_token = response.json().get('access_token')
+    # This access_token is what we'll use to make requests in the following
+    # steps
+    access_token = response.json().get('access_token')

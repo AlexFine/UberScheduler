8fee831e69ac4e5bd9fd352759b2acbdffe1e279
Sam Schickler <gadgetsam@gmail.com>
2016-03-12 11:15:06 -0800

    added all api's and commented all

--- Python Server/Uber/uber_api.py
+++ Python Server/Uber/uber_api.py
@@ -1,7 +1,4 @@
-"""Hello World API implemented using Google Cloud Endpoints.
-
-Defined here are the ProtoRPC messages needed to define Schemas for methods
-as well as those methods defined in an API.
+"""Uber_APi uses google cloud endpoints
 """
 
 import endpoints
@@ -12,14 +9,24 @@ from datastore import UserRideDataBase
 import time
 from google.appengine.api import urlfetch
 # from flask import Flask
-import unicodedata
+"""These area is for files with the api methods
+ride_api:  it also will contain all function for get rides
+in next 30 minutes
+user_api: this for interacting with the data store for users
+time_api: these are small functions that relate to when an uber is coming and time differences"""
+from ride_api import *
+from user_api import *
+from time_api import *
+
+
+# import unicodedata
 from auth_1 import auth_step_one
 from auth_2 import auth_step_two
 
 datastores = UserRideDataBase()
 package = 'Hello'
 
-
+"""These are classes for the inputs and returns  for post and get requests"""
 class Greeting(messages.Message):
     """Greeting that stores a message."""
     message = messages.StringField(1)
@@ -37,7 +44,7 @@ class timeMin(messages.Message):
     min = messages.StringField(1)
 
 
-class user(messages.Message):  # this is input and response class for user
+class User(messages.Message):  # this is input and response class for user
     """Greeting that stores a message."""
     # key = messages.StringField(2, required=True)
     code = messages.IntegerField(1, variant=messages.Variant.INT32)
@@ -56,7 +63,8 @@ class Ride(messages.Message):
     slat = messages.FloatField(3, required=True)
     elong = messages.FloatField(4, required=True)
     elat = messages.FloatField(5, required=True)
-    time = messages.StringField(6, required=True)
+    time = messages.IntegerField(6, required=True)
+    userKey = messages.IntegerField(7, variant=messages.Variant.INT32)
     # rid = messages.StringField(8, required=True) #ride ID
 
 STORED_GREETINGS = GreetingCollection(items=[
@@ -67,61 +75,39 @@ STORED_GREETINGS = GreetingCollection(items=[
 
 @endpoints.api(name='uberApi', version='v1')
 class UberApi(remote.Service):
-    rideReturn = endpoints.ResourceContainer(
-        keySearch
-        # userkey
-    )  # defines resources in post request
 
+    """these are the resource containers"""
+    userReturn = endpoints.ResourceContainer( keySearch)
+    rideReturn = endpoints.ResourceContainer(keySearch) #these are different resourse containers for the post requests
+    rideCreate = endpoints.ResourceContainer(Ride)
+    createUser = endpoints.ResourceContainer(User)
+    noInput= endpoints.ResourceContainer(message_types.VoidMessage)#this one is for ones with no input
+    timeGet = endpoints.ResourceContainer(
+        message_types.VoidMessage,
+
+        slat=messages.FloatField(1, variant=messages.Variant.FLOAT),
+        slong=messages.FloatField(2, variant=messages.Variant.FLOAT),
+
+    )
+    """This is the rides area"""
     @endpoints.method(rideReturn, Ride,
                       path='datastore/returnRide', http_method='POST',
                       name='ride.return')  # defines url and type of request
     def returnRide(self, request):
-        ride = datastores.returnRide(request.key)
-        print ride
-        return Ride(ukey=ride[0], slong=ride[1], slat=ride[2], elong=ride[3], elat=ride[4], time=ride[5])
-        # try:
-        # print datastores.returnUser(request.key)
-
-          # returns in json format
-        # except:
-        # return Greeting(message="not in database")
-    rideCreate = endpoints.ResourceContainer(
-        Ride
-        # userkey
-    )  # defines resources in post request
+        return returnRideApi(request)
 
     @endpoints.method(rideCreate, keySearch, path='datastore/createRide', http_method='POST', name='ride.create')  # defines url and type of request
     def createRide(self, request):
-        ride = datastores.createRide(request.ukey, request.slong, request.slat, request.elong, request.elat, request.time)
-        return keySearch(key=int(ride))
-    noInput= endpoints.ResourceContainer(message_types.VoidMessage)
-    # Authorization
-    userReturn = endpoints.ResourceContainer(
-        # Greeting
-        keySearch
-    )  # defines resources in post request
-    userReturn = endpoints.ResourceContainer(
-        # Greeting
-        keySearch
-    )  # defines resources in post request
-
+        return createRideApi(request)
     @endpoints.method(userReturn, Greeting,
-                      path='datastore/allReturnRide', http_method='POST',
+                        path='datastore/allReturnRide', http_method='POST',
                       name='rides.returnAll')  # defines url and type of request
 
     def allReturnRide(self, request):
-        # try:
-        # print datastores.returnUser(request.key)
+          return allReturnRideApi(request)
 
-        users = datastores.returnAllRides(request.key)  # pings data store api
-        print users
-
-
-
-        return Greeting(str(users))  # returns in json format
-        # except:
-        # return Greeting(message="not in database")
-
+    """This is the end of the rides area
+    Start of auth area"""
     @endpoints.method(noInput, Greeting, path='authorize/stepone', http_method='GET', name='auth.one')
     def redirectUrl(self, request):
         reurl = auth_step_one()
@@ -134,123 +120,28 @@ class UberApi(remote.Service):
         return Greeting(message=realtoken)
         #use createUser to make a new user
 
+    """End of Auth start of User area"""
 
-
-
-
-    @endpoints.method(userReturn, user,
+    @endpoints.method(userReturn, User,
                       path='datastore/returnUser', http_method='POST',
-                      name='user.return')  # defines url and type of request
-
+                      name='user.return')
     def returnUser(self, request):
-        # try:
-        # print datastores.returnUser(request.key)
-
-        users = datastores.returnUser(request.key)  # pings data store api
-        print user
-        print "test 2"
-        email = str(unicodedata.normalize('NFKD', users[0]).encode('ascii', 'ignore')) #turns unicode to ascii
-        pswd = str(unicodedata.normalize('NFKD', users[1]).encode('ascii', 'ignore'))
-        code = int(unicodedata.normalize('NFKD', users[2]).encode('ascii', 'ignore'))
-
-        return user(email=email, pswd=pswd, code=code)  # returns in json format
+        return returnUserApi(request)
         # except:
         # return Greeting(message="not in database")
 
-    createUser = endpoints.ResourceContainer(
-        user
-        # code=messages.IntegerField(1, variant=messages.Variant.INT32),email = messages.StringField(2, required=True),pswd = messages.StringField(3, required=True))
-    )
+
 
     @endpoints.method(createUser, keySearch,
                       path='datastore/usercreate', http_method='POST',
                       name='user.create')
     def userCreate(self, request):
-        userkey = datastores.createUser(request.email, request.pswd,
-                                        str(request.code))  # create user from requests object
-
-        return keySearch(key=userkey)  # returns userkey to frontend
-
-
-    def compare(pickuptime, pickupdate):
-
-        spt = pickuptime.split(":")
-        dhour = float(spt[0])
-        dmin = float(spt[1])
-        dtmins = (dhour * 60) + (dmin)
-
-        sdt = pickupdate.split("/")
-        dmonth = float(sdt[0])
-        ddate = float(sdt[1])
-        dyear = float(sdt[2])
-        hour = float(time.strftime('%H')) * 60
-        min = float(time.strftime('%M'))
-        day = float(time.strftime('%d'))
-        month = float(time.strftime('%m'))
-        year = float(time.strftime('%y'))
-
-        tmins = hour + min                  #
-
-        diff = dtmins - tmins
-
-
-    ID_RESOURCE3 = endpoints.ResourceContainer(
-        message_types.VoidMessage,
-
-        slat=messages.FloatField(1, variant=messages.Variant.FLOAT),
-        slong=messages.FloatField(2, variant=messages.Variant.FLOAT),
-
-    )
-
-    @endpoints.method(ID_RESOURCE3, timeMin,
+        return userCreateApi(request)
+    """time Api area"""
+    @endpoints.method(timeGet, timeMin,
                       path='uber/getTime', http_method='GET',
                       name='uber.getTime')
     def gettime(self, request):
-        # slat = ""
-        # slong = ""
-
-        slat = request.slat
-        slong = request.slong
-        # elat = 37.791948
-        # elong = -122.446480
-
-        baseurl = "https://sandbox-api.uber.com/v1/estimates/time"
-
-        parameters = {
-            'server_token': 'ikGvlAJSejPSY6bUp7APhxkwyu5ermguZnreUaCd',
-            'start_latitude': str(slat),
-            'start_longitude': str(slong),
-        }
-
-        url = baseurl + "?" + "server_token=" + parameters['server_token'] + "&start_latitude=" + parameters[
-            'start_latitude'] + "&start_longitude=" + parameters['start_longitude']
-
-        # url = "https://sandbox-api.uber.com/v1/estimates/time?server_token=ikGvlAJSejPSY6bUp7APhxkwyu5ermguZnreUaCd&start_latitude=37.775818&start_longitude=-122.418028"
-        result = urlfetch.fetch(url)
-        if result.status_code == 200:
-            print result.content
-            return timeMin(min=str(result.content))
-        else:
-            return None
-            # data = str(response.json())
-            # ux = data.split("uberX")
-            # uxe = ux[1].split(":")
-            # uxef = uxe[1].split(",")
-            # estimate = float(uxef[0])
-            # print(estimate) #estimate in seconds
-            # return estimate
-        # ID_RESOURCE3 = endpoints.ResourceContainer(
-        #     message_types.VoidMessage,
-        #
-        #     slat=messages.FloatField(1, variant=messages.Variant.FLOAT),
-        #     slong=messages.FloatField(2, variant=messages.Variant.FLOAT),
-        #
-        # )
-        #
-        # @endpoints.method(ID_RESOURCE3, timeMin,
-        #                   path='uber/getTime', http_method='GET',
-        #                   name='uber.getTime')
-        # def gettime(self, request):
-
+        return getTimeApi(request)
 
 APPLICATION = endpoints.api_server([UberApi])
---
+++ Python Server/Uber/user_api.py
@@ -0,0 +1,32 @@
+"""user_api: this for interacting with the data store for users"""
+from datastore import UserRideDataBase
+import endpoints
+from protorpc import messages
+from protorpc import message_types
+from protorpc import remote
+from datastore import UserRideDataBase
+import time
+from google.appengine.api import urlfetch
+# from flask import Flask
+import uber_api
+from ride_api import *
+import unicodedata
+datastores = UserRideDataBase()
+def returnUserApi(self, request):
+        # try:
+        # print datastores.returnUser(request.key)
+
+        users = datastores.returnUser(request.key)  # pings data store api
+        # print User
+        print "test 2"
+        email = str(unicodedata.normalize('NFKD', users[0]).encode('ascii', 'ignore')) #turns unicode to ascii
+        pswd = str(unicodedata.normalize('NFKD', users[1]).encode('ascii', 'ignore'))
+        code = int(unicodedata.normalize('NFKD', users[2]).encode('ascii', 'ignore'))
+
+        return uber_api.User(email=email, pswd=pswd, code=code)  # returns in json format
+
+def userCreateApi(self, request):
+        userkey = datastores.createUser(request.email, request.pswd,
+                                        str(request.code))  # create user from requests object
+
+        return uber_api.keySearch(key=userkey)  # returns userkey to frontend

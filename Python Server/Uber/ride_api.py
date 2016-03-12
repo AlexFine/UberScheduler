03472be46102d64b7adb15f2ea7b521e5e4a4b53
Sam Schickler <gadgetsam@gmail.com>
2016-03-12 11:14:14 -0800

    Revert "added all api's"
    This reverts commit a088e696f1697829d74dfa31944f2d30899ac6a7.

--- Python Server/Uber/ride_api.py
+++ Python Server/Uber/ride_api.py
@@ -16,9 +16,3 @@ def returnRideApi(request):
         ride = datastores.returnRide(request.key)
         print ride
         return uber_api.Ride(ukey=ride[0], slong=ride[1], slat=ride[2], elong=ride[3], elat=ride[4], time=ride[5])
-def createRideApi(request):
-        ride = datastores.createRide(request.ukey, request.slong, request.slat, request.elong, request.elat, request.time, request.userKey)
-        return uber_api.keySearch(key=int(ride))
-def allReturnRideApi(self, request):
-        users = datastores.returnAllRides(request.key)  # pings data store api
-        return uber_api.Greeting(str(users))
